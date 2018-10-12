f2a64afff0107e072ea6c0f415318f75cc9b077c
Nattarat Srila <nattarat.design@gmail.com>
2018-10-12 18:27:16 +0700

    create Bootstrap component for explain how to using other CSS framework in project

---
+++ src/components/Bootstrap/Bootstrap.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ClassNames from 'classnames'
+import PropTypes from 'prop-types'
+import {
+  BootstrapWrapper
+} from './styled'
+
+/**
+ * Bootstrap description
+ * - Bootstrap v4.1.3 (https://github.com/twbs/bootstrap)
+ * - Class components:
+ *    - Grids (https://getbootstrap.com/docs/4.1/layout/grid/)
+ */
+
+export class Bootstrap extends React.PureComponent {
+  static propTypes = { // TYPE > node, string, func, bool
+    /**
+    * [Bootstrap] and [Bootstrap.Children] - additional elements or text
+    */
+    children: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.string
+    ])
+  }
+
+  render () {
+    const {
+      children
+    } = this.props
+
+    // props for css classes
+    const classes = ClassNames(
+      'bootstrap'
+    )
+
+    return (
+      <BootstrapWrapper
+        className={classes}
+      >
+        {children}
+      </BootstrapWrapper>
+    )
+  }
+}
---
+++ src/components/Bootstrap/Readme.md
@@ -0,0 +1,19 @@
+Grids:
+
+```jsx
+<Bootstrap>
+  <div class='container'>
+    <div class='row'>
+      <div class='col-sm'>
+        <div style={{ backgroundColor: 'gray' }}>One of three columns</div>
+      </div>
+      <div class='col-sm'>
+        <div style={{ backgroundColor: 'gray' }}>One of three columns</div>
+      </div>
+      <div class='col-sm'>
+        <div style={{ backgroundColor: 'gray' }}>One of three columns</div>
+      </div>
+    </div>
+  </div>
+</Bootstrap>
+```
